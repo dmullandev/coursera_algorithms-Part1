dce65ce11a5c3731fcdc9ab49bc09fb758bebe8a
dmullrod <dwmullan@gmail.com>
2018-02-12 18:52:39 +0000

    assignment3 base classes

---
+++ src/assignment3/BruteCollinearPoints.java
@@ -0,0 +1,15 @@
+package assignment3;
+
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {
+        
+    }
+    
+    public int numberOfSegments() {
+        
+    }
+    
+    public LineSegment[] segments() {
+        
+    }
+}
---
+++ src/assignment3/FastCollinearPoints.java
@@ -0,0 +1,15 @@
+package assignment3;
+
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+        
+    }
+    
+    public int numberOfSegments() {
+        
+    }
+    
+    public LineSegment[] segments() {
+        
+    }
+}
