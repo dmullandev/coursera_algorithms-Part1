1c458c34183e5f069b767f861df7fdf06edf1067
dmullrod <dwmullan@gmail.com>
2018-02-14 17:59:21 +0000

    point compareTo()

--- src/assignment3/Point.java
+++ src/assignment3/Point.java
@@ -1,7 +1,10 @@
 package assignment3;
 public class Point implements Comparable<Point>{
+    private int x;
+    private int y;
     public Point(int x, int y) {
-        
+        this.x = x;
+        this.y = y;
     }
     
     public void draw() {
@@ -17,7 +20,9 @@ public class Point implements Comparable<Point>{
     }
     
     public int compareTo(Point that) {
-        
+        if (that.y < this.y || (that.y == this.y && that.x < this.x) ) return -1; // if that is less
+        if (that.y > this.y || (that.y == this.y && that.x > this.x) ) return +1; // if that is greater
+        return 0;   // if that is equal
     }
     
     public double slopeTo(Point that) {
