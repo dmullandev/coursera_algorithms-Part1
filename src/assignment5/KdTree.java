dd641e83c2396b97f1bf1d7085780be257669a1e
dmullrod <dwmullan@gmail.com>
2018-02-26 14:39:36 +0000

    assignment5 base classes

---
+++ src/assignment5/KdTree.java
@@ -0,0 +1,32 @@
+package assignment5;
+
+public class KdTree {
+    public KdTree() {}
+    public boolean isEmpty() {
+        
+    }
+    
+    public int size() {
+        
+    }
+    
+    public void insert(Point2D p) {
+        
+    }
+    
+    public boolean containts(Point2D p) {
+        
+    }
+    
+    public void draw() {
+        
+    }
+    
+    public Iterable<Point2D> range(RectHV rect) {
+        
+    }
+    
+    public Point2D nearest(Point2D p) {
+        
+    }
+}
